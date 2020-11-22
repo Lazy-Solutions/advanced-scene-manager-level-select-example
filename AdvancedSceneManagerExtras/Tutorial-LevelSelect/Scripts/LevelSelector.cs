797aa6fc54bd1a73f3c46c10900d09c109081fbb
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-11-22 10:42:34 +0100

    Pushed tutorial

---
+++ AdvancedSceneManagerExtras/Tutorial-LevelSelect/Scripts/LevelSelector.cs
@@ -0,0 +1,22 @@
+﻿using AdvancedSceneManager.Callbacks;
+using AdvancedSceneManager.Models;
+using System.Linq;
+using UnityEngine;
+
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        var Search = Profile.current.collections.Where(c => c.name.Contains("Level_"));
+
+        foreach (var item in Search)
+        {
+            GameObject newbutton = Instantiate(prefab, transform);
+            ExtraDataASM data = (ExtraDataASM)item.extraData;
+            newbutton.GetComponent<LevelSelectorButton>().ApplyData(data.thumbnail, data.levelName, item);
+        }
+    }
+}
